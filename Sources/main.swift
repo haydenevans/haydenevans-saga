8da4db79fd978df1e30f5ccec8242ecc49b1f349
Hayden Evans <hayden.evans@gmail.com>
2025-05-19 13:25:44 +0000

    Updates main.swift

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,4 +1,42 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
-print("Hello, world!")
+import Foundation
+import Saga
+import SagaParsleyMarkdownReader
+import SagaSwimRenderer
+import HTML
+
+
+func renderPage(context: ItemRenderingContext<EmptyMetadata>) -> Node {
+  html(lang: "en-US") {
+    body {
+      div(id: "content") {
+        h1 { context.item.title }
+        Node.raw(context.item.body)
+      }
+    }
+  }
+}
+
+
+@main
+struct Run {
+  static func main() async throws {
+    try await Saga(input: "content", output: "deploy")
+      // All Markdown files within the `input` folder will be parsed to html.
+      .register(
+        readers: [.parsleyMarkdownReader],
+        writers: [.itemWriter(swim(renderPage))]
+      )
+
+
+      // Run the step we registered above
+      .run()
+
+
+      // All the remaining files that were not parsed from markdown, so for example 
+      // images, raw html files and css, are copied as-is to the output folder.
+      .staticFiles()
+  }
+}
